caa8bf127326f1df85aee044b3eee6f864db0fbd
pejmanS21 <pezhmansamadi21@gmail.com>
2022-02-22 11:59:15 +0330

    use save_image instead of plt

--- src/inc/IO/grad_output.py
+++ src/inc/IO/grad_output.py
@@ -2,6 +2,7 @@ from gradcam.utils import visualize_cam
 from gradcam import GradCAM, GradCAMpp
 import numpy as np
 import matplotlib.pyplot as plt
+from torchvision.utils import save_image
 import torch
 from ..config.utils import stats, classes
 
@@ -26,7 +27,6 @@ def visualizer(image, model, model_type: str = 'resnet', result_path: str = '../
         print('Check input Model!')
         return 0
 
-
     if len(image.size()) != 4:
         image = image.unsqueeze(0)
 
@@ -40,17 +40,24 @@ def visualizer(image, model, model_type: str = 'resnet', result_path: str = '../
     heatmap, result = visualize_cam(mask, image)
     mask_pp, _ = gradcam_pp(image)
     heatmap_pp, result_pp = visualize_cam(mask_pp, image)
-
-    fig, axs = plt.subplots(1, 3)
-    axs[0].imshow(denormalizer(image[0]))
-    axs[1].imshow(result.permute(1, 2, 0))
-    axs[2].imshow(result_pp.permute(1, 2, 0))
-
-    axs[0].axis('off')
-    axs[1].axis('off')
-    axs[2].axis('off')
     
-    fig.suptitle(classes[pred[0]])
-    plt.savefig(result_path)
+    save_fig(image[0], result, result_pp, result_path)
+
+    # fig, axs = plt.subplots(1, 3)
+    # axs[0].imshow(denormalizer(image[0]))
+    # axs[1].imshow(result.permute(1, 2, 0))
+    # axs[2].imshow(result_pp.permute(1, 2, 0))
+
+    # axs[0].axis('off')
+    # axs[1].axis('off')
+    # axs[2].axis('off')
+    
+    # fig.suptitle(classes[pred[0]])
+    # plt.savefig(result_path)
     
     return pred[0], classes[pred[0]]
+
+def save_fig(image, grad_image, grad_image_pp, results_path):
+    final_image = torch.cat((torch.from_numpy(denormalizer(image).transpose(2, 0, 1)), grad_image, grad_image_pp), 2)
+    save_image(final_image, results_path)
+
